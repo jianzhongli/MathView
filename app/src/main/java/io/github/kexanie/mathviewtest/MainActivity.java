f5c791c85aa34590ecfb4dcb3db8f5967e8885da
kexanie <key.insist@gmail.com>
2015-12-02 17:32:57 +0800

    update test example

--- app/src/main/java/io/github/kexanie/mathviewtest/MainActivity.java
+++ app/src/main/java/io/github/kexanie/mathviewtest/MainActivity.java
@@ -1,20 +1,18 @@
 package io.github.kexanie.mathviewtest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import io.github.kexanie.library.MathView;
 
 public class MainActivity extends AppCompatActivity {
-    MathView math_one;
-    MathView math_two;
-    MathView font_test;
+    MathView formula_one;
+    MathView formula_two;
     String tex = "This come from string. You can insert inline formula:" +
-            " \\(\\sum_{i=0}^n i^2 = \\frac{(n^2+n)(2n+1)}{6}\\) " +
+            " \\(ax^2 + bx + c = 0\\) " +
             "or displayed formula: $$\\sum_{i=0}^n i^2 = \\frac{(n^2+n)(2n+1)}{6}$$";
 
     @Override
@@ -27,24 +25,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
-        math_one = (MathView) findViewById(R.id.formula_one);
-        math_two = (MathView) findViewById(R.id.formula_two);
-        font_test = (MathView) findViewById(R.id.font_test);
+        formula_one = (MathView) findViewById(R.id.formula_one);
+        formula_two = (MathView) findViewById(R.id.formula_two);
 
         // This toast will print raw TeX string
-        Toast.makeText(this, math_one.getText(), Toast.LENGTH_LONG).show();
-        math_two.setText(tex);
-        font_test.setText(
-                "\\(Greek letters: \\alpha, \\beta, …, \\omega, \\Gamma, \\Delta, …, \\Omega\\)" +
-                "\\(Superscipt and subscripts: x_i^2\\)" +
-                "\\(\\mathbf{Sums and integrals}: \\sum_1^n, \\sum_{i=0}^\\infty i^2, \\prod \\\\" +
-                        " \\int, \\bigcup, \\bigcap, \\iint\\)" +
-                "\\(\\mathbf{Fractions}: \\frac ab, \\frac{a+1}{b+1}, {a+1\\over b+1}\\)" +
-                        "\\(\\mathbf{Boldface}: \\mathbf{ABCDEFGHIJKLMNOPQRSTUVWXYZ}\\)" +
-                        "\\(\\mathtt{Typewriter font}: \\mathtt{ABCDEFGHIJKLMNOPQRSTUVWXYZ}\\)" +
-                        "\\(\\mathrm{Roman font}: \\mathrm{ABCDEFGHIJKLMNOPQRSTUVWXYZ}\\)" +
-                        "\\(\\mathcal{Calligraphic}: \\mathcal{ABCDEFGHIJKLMNOPQRSTUVWXYZ}\\)" +
-                        "\\(\\mathfrak {Fraktur letters}: \\mathfrak {ABCDEFGHIJKLMNOPQRSTUVWXYZ}\\)");
+        Toast.makeText(this, formula_one.getText(), Toast.LENGTH_LONG).show();
+        formula_two.setText(tex);
     }
 
     @Override
