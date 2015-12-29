b35b0c07f2c77b8be64a32dbd6f5a4ffb0482c8f
kexanie <key.insist@gmail.com>
2015-12-29 20:22:54 +0800

    add support for MathML

--- app/src/main/java/io/github/kexanie/mathviewtest/MainActivity.java
+++ app/src/main/java/io/github/kexanie/mathviewtest/MainActivity.java
@@ -9,11 +9,54 @@ import android.widget.Toast;
 import io.github.kexanie.library.MathView;
 
 public class MainActivity extends AppCompatActivity {
-    MathView formula_one;
     MathView formula_two;
+    MathView formula_three;
     String tex = "This come from string. You can insert inline formula:" +
             " \\(ax^2 + bx + c = 0\\) " +
             "or displayed formula: $$\\sum_{i=0}^n i^2 = \\frac{(n^2+n)(2n+1)}{6}$$";
+    String mathml =
+            "<math xmlns=\"http://www.w3.org/1998/Math/MathML\" display=\"block\" mathcolor=\"black\">\n" +
+            "  <mrow>\n" +
+            "    <mi>f</mi>\n" +
+            "    <mrow>\n" +
+            "      <mo>(</mo>\n" +
+            "      <mi>a</mi>\n" +
+            "      <mo>)</mo>\n" +
+            "    </mrow>\n" +
+            "  </mrow>\n" +
+            "  <mo>=</mo>\n" +
+            "  <mrow>\n" +
+            "    <mfrac>\n" +
+            "      <mn>1</mn>\n" +
+            "      <mrow>\n" +
+            "        <mn>2</mn>\n" +
+            "        <mi>&#x3C0;</mi>\n" +
+            "        <mi>i</mi>\n" +
+            "      </mrow>\n" +
+            "    </mfrac>\n" +
+            "    <msub>\n" +
+            "      <mo>&#x222E;</mo>\n" +
+            "      <mrow>\n" +
+            "        <mi>&#x3B3;</mi>\n" +
+            "      </mrow>\n" +
+            "    </msub>\n" +
+            "    <mfrac>\n" +
+            "      <mrow>\n" +
+            "        <mi>f</mi>\n" +
+            "        <mo>(</mo>\n" +
+            "        <mi>z</mi>\n" +
+            "        <mo>)</mo>\n" +
+            "      </mrow>\n" +
+            "      <mrow>\n" +
+            "        <mi>z</mi>\n" +
+            "        <mo>&#x2212;</mo>\n" +
+            "        <mi>a</mi>\n" +
+            "      </mrow>\n" +
+            "    </mfrac>\n" +
+            "    <mi>d</mi>\n" +
+            "    <mi>z</mi>\n" +
+            "  </mrow>\n" +
+            "</math>";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +68,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
-        formula_one = (MathView) findViewById(R.id.formula_one);
         formula_two = (MathView) findViewById(R.id.formula_two);
+        formula_three = (MathView) findViewById(R.id.formula_three);
 
-        // This toast will print raw TeX string
-        Toast.makeText(this, formula_one.getText(), Toast.LENGTH_LONG).show();
         formula_two.setText(tex);
+        formula_three.setText(mathml);
     }
 
     @Override
