907f97cef441244241ecfc2bc4fc57a76fc2c4a1
duy <tranleduy1233@gmail.com>
2019-07-01 00:40:54 +0700

    "Disable touch event" attr

--- MathView/src/main/java/io/github/kexanie/library/MathView.java
+++ MathView/src/main/java/io/github/kexanie/library/MathView.java
@@ -4,9 +4,7 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
-import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
@@ -18,6 +16,8 @@ public class MathView extends WebView {
     private String mText;
     private String mConfig;
     private int mEngine;
+    private boolean mDarkTextColor = true;
+    private boolean mDisableTouchEvent;
 
     public MathView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -25,25 +25,27 @@ public class MathView extends WebView {
         getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
         setBackgroundColor(Color.TRANSPARENT);
 
-        TypedArray mTypeArray = context.getTheme().obtainStyledAttributes(
+        TypedArray typeArray = context.getTheme().obtainStyledAttributes(
                 attrs,
                 R.styleable.MathView,
                 0, 0
         );
 
         try { // the order of execution of setEngine() and setText() matters
-            setEngine(mTypeArray.getInteger(R.styleable.MathView_engine, 0));
-            setText(mTypeArray.getString(R.styleable.MathView_text));
+            setDisableTouchEvent(typeArray.getBoolean(R.styleable.MathView_disableTouchEvent, true));
+            setEngine(typeArray.getInteger(R.styleable.MathView_engine, 0));
+            setText(typeArray.getString(R.styleable.MathView_text));
         } finally {
-            mTypeArray.recycle();
+            typeArray.recycle();
         }
     }
 
     // disable touch event on MathView
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        return false;
+        return mDisableTouchEvent;
     }
+
 
     private Chunk getChunk() {
         String TEMPLATE_KATEX = "katex";
@@ -51,8 +53,12 @@ public class MathView extends WebView {
         String template = TEMPLATE_KATEX;
         AndroidTemplates loader = new AndroidTemplates(getContext());
         switch (mEngine) {
-            case Engine.KATEX: template = TEMPLATE_KATEX; break;
-            case Engine.MATHJAX: template = TEMPLATE_MATHJAX; break;
+            case Engine.KATEX:
+                template = TEMPLATE_KATEX;
+                break;
+            case Engine.MATHJAX:
+                template = TEMPLATE_MATHJAX;
+                break;
         }
 
         return new Theme(loader).makeChunk(template);
@@ -71,6 +77,10 @@ public class MathView extends WebView {
 
     public String getText() {
         return mText;
+    }
+
+    public void setDisableTouchEvent(boolean disableTouchEvent) {
+        this.mDisableTouchEvent = disableTouchEvent;
     }
 
     /**
@@ -109,7 +119,8 @@ public class MathView extends WebView {
                 mEngine = Engine.MATHJAX;
                 break;
             }
-            default: mEngine = Engine.KATEX;
+            default:
+                mEngine = Engine.KATEX;
         }
     }
 
